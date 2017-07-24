4bbb9fd30dd2f3cd8e47eec7a00b439dec1765c9
Peter Hafid <peter@ctrl-c.liu.se>
2017-07-24 11:06:23 +0200

    Add custom javascript

---
+++ main.js
@@ -0,0 +1,26 @@
+$( "#submit" ).click(function(e) {
+  e.preventDefault();
+  alert("This form is for demonstration only please use regular email instead");
+});
+
+$( ".fa" ).click(function() {
+  const FCCURL = "https://www.freecodecamp.org/pethaf",
+        GITHUBURL = "https://www.github.com/pethaf",
+        CODEPENURL = "https://codepen.io/pethaf/";
+  if(this.classList.value.indexOf("free-code-camp") != -1)
+  {
+    window.open(FCCURL,'_blank');
+  }
+  else if(this.classList.value.indexOf("github") != -1)
+  {
+    window.open(GITHUBURL,'_blank');
+  }
+  else if(this.classList.value.indexOf("codepen") !=-1)
+  {
+      window.open(CODEPENURL,'_blank');
+
+  }
+
+  console.log(this);
+  console.log(this.classList.value.indexOf("free-code-camp"));
+});
